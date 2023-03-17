5b30bc14c9d51a926e2da46f4a813017e52e0aab
Walter McCain III <param2002@outlook.com>
2023-03-17 06:01:05 +0000

    update code

--- PipelineProject.py
+++ PipelineProject.py
@@ -37,7 +37,21 @@ for sample in samples:
     os.system(f"kallisto index -i {sample}.idx {sample}_1.fastq {sample}_2.fastq")
     os.system(f"kallisto quant -i {sample}.idx -o {sample}_output -t 4 {sample}_1.fastq {sample}_2.fastq")
     with open(f"{sample}_output/abundance.tsv", "w") as f:
-        f.write("target_id\test_counts\ttpm\n")
+        f.write("target_id\ttest_stat\tpval\tqval\n")
+
+os.system("Rscript -e 'library(sleuth); metadata = data.frame(sample = c('SRR5660030', 'SRR5660033', 'SRR5660044', 'SRR5660045'), condition = c('2dpi', '2dpi', '6dpi', '6dpi')); so = sleuth_prep(metadata, ~condition, extra_bootstrap_summary=T); so = sleuth_fit(so); so = sleuth_fit(so, 'full'); res = sleuth_results(so, '2dpi', '6dpi'); write.table(res[res$qval<0.05,c('target_id','test_stat','pval','qval')], file='significant_transcripts.txt', sep='\t', row.names=F, quote=F)'")
+
+with open("log.txt", "w") as f:
+    f.write("target_id\ttest_stat\tpval\tqval\n")
+with open("significant_transcripts.txt") as st:
+    next(st)
+    for line in st:
+        fields = line.strip().split("\t")
+        target_id = fields[0]
+        test_stat = fields[1]
+        pval = fields[2]
+        qval = fields[3]
+        f.write(f"{target_id}\t{test_stat}\t{pval}\t{qval}\n")
 
 tpm_data = {}
 for sample in samples:
